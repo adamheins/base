67c79baf5ebae9455095f855bf6ef390425468a6
Adam Heins <mail@adamheins.com>
2015-12-12 23:54:31 -0800

    Only print success message if file successfully copied.

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 if [ $(which clisp) ]; then
-  cp base.lisp /usr/local/bin/base
-  echo "base installed to /usr/local/bin/base"
+  cp base.lisp /usr/local/bin/base && echo "base installed to /usr/local/bin/base"
 else
   echo "clisp is not installed. clisp must be installed to run base. Aborting."
 fi
